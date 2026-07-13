a7701ac8bda79539ab698a893318c722783e5f1d
zacbailey <Zacbailey987@gmail.com>
2026-07-13 15:22:35 -0700

    UserRepository set up

--- backend/src/main/java/com/minitube/users/UserRepository.java
+++ backend/src/main/java/com/minitube/users/UserRepository.java
@@ -1,5 +1,13 @@
 package com.minitube.users;
 
-public class UserRepository {
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByUsername(String username);
 
 }
